36e689a51cdafe93096166de33d5978b494af00b
Linus <wendefelix1998@gmail.com>
2017-12-02 06:37:07 +0100

    Overhauled and commented code for Day 2

--- puzzle2.py
+++ puzzle2.py
@@ -3,32 +3,29 @@ from itertools import permutations
 log = None
 
 
-def get_rows(puzzle_input):
-    return tuple(puzzle_input.split('\n'))
-
-
 def solve_part_1(puzzle_input):
     checksum_total = 0
-    puzzle_input = puzzle_input.replace(' ', '\t')
-    for row in get_rows(puzzle_input):
+    puzzle_input = puzzle_input.replace(' ', '\t')  # Helpful for the examples
+    for row in puzzle_input.split('\n'):
         entries = list(int(val) for val in row.split('\t'))
-        entries.sort()
-        checksum = (entries[-1] - entries[0])
-        log('Checksum: %d' % checksum)
-        checksum_total += checksum
-    return checksum_total
+        entries.sort()  # Ints are sorted ascending e.g. [1 (Min), ..., 3 (Max)]
+        checksum_row = (entries[-1] - entries[0])  # Checksum is: Max - Min
+        log('Row-Checksum: %d' % checksum_row)
+        checksum_total += checksum_row
+    return checksum_total  # The solution is the total of all checksums
 
 
 def solve_part_2(puzzle_input):
     checksum_total = 0
-    puzzle_input = puzzle_input.replace(' ', '\t')
-    for row in get_rows(puzzle_input):
+    puzzle_input = puzzle_input.replace(' ', '\t')  # Helpful for the examples
+    for row in puzzle_input.split('\n'):
         entries = list(int(val) for val in row.split('\t'))
-        entries.sort()
-        checksum = 0
-        for v1, v2 in permutations(entries, 2):
-            if v1 % v2 is 0:
-                checksum = v1 // v2
-        log('Checksum: %d' % checksum)
-        checksum_total += checksum
-    return checksum_total
+        checksum_row = 0  # Default checksum
+        for entry1, entry2 in permutations(entries, 2):
+            if entry1 % entry2 is 0:  # Check if entry1 can be devided by entry2
+                checksum_row = entry1 // entry2  # <-- The checksum
+        else:  # No checksum found
+            raise Exception('Could not find any checksum for row: "%s"' % row)
+        log('Row-Checksum: %d' % checksum_row)
+        checksum_total += checksum_row
+    return checksum_total  # The solution is the total of all checksums
