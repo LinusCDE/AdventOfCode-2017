802a468861525d1867faaf8607738283028ea524
Linus <wendefelix1998@gmail.com>
2017-12-02 18:43:13 +0100

    Overhauled solution for day 2

--- puzzle2.py
+++ puzzle2.py
@@ -7,9 +7,8 @@ def solve_part_1(puzzle_input):
     checksum_total = 0
     puzzle_input = puzzle_input.replace(' ', '\t')  # Helpful for the examples
     for row in puzzle_input.split('\n'):
-        entries = list(int(val) for val in row.split('\t'))
-        entries.sort()  # Ints are sorted ascending e.g. [1 (Min), ..., 3 (Max)]
-        checksum_row = (entries[-1] - entries[0])  # Checksum is: Max - Min
+        entries = tuple(map(int, row.split('\t')))  # Entries as ints
+        checksum_row = (max(entries) - min(entries))  # + checksum for cur. row
         log('Row-Checksum: %d' % checksum_row)
         checksum_total += checksum_row
     return checksum_total  # The solution is the total of all checksums
@@ -19,13 +18,12 @@ def solve_part_2(puzzle_input):
     checksum_total = 0
     puzzle_input = puzzle_input.replace(' ', '\t')  # Helpful for the examples
     for row in puzzle_input.split('\n'):
-        entries = list(int(val) for val in row.split('\t'))
-        checksum_row = 0  # Default checksum
+        entries = tuple(map(int, row.split('\t')))   # Entries as ints
         for entry1, entry2 in permutations(entries, 2):
             if entry1 % entry2 is 0:
-                checksum_row = entry1 // entry2  # <-- The checksum
+                checksum_row = entry1 // entry2  # + checksum for cur. row
                 break
-        else:  # No checksum found
+        else:  # If no checksum found:
             raise Exception('Could not find any checksum for row: "%s"' % row)
         log('Row-Checksum: %d' % checksum_row)
         checksum_total += checksum_row
