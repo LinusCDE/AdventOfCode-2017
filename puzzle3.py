ec42f578e2fcd664bbefeaedf30fac0244980eab
Linus <wendefelix1998@gmail.com>
2017-12-03 08:43:33 +0100

    Did a lot of improvements
    - Split some parts into functions or constants to be more readable (e.g. vector_turn_left, VEC_<Direction>)
    - Overhauled distance_function with inline-if-statements instead of the usage of min() and max() which had a performance improvement of about 33%.
    - Allowing the spiral()-generator to skip all locations expect the final postions gave a performance gain of about 57%
    - Making the dict in solve_part_2() into a 2D-Matrix - and using respective functions to put and set the values - nearly doubled the speed
    - Removing log messages also did a lot of improvement in terms of speed (even if verbosity wasn't enabled)
    - Positions are now tuples instead of lists
    - Overhauled naming of many variables
    
    Overall part1 got about 84 times faster (84 ms to just beyond 1 ms) and part two was nearly doubled in speed.

--- puzzle3.py
+++ puzzle3.py
@@ -1,12 +1,14 @@
 log = None
 
 
-def distance(pos1, pos2):
-    x_dist = max(pos1[0], pos2[0]) - min(pos1[0], pos2[0])
-    y_dist = max(pos1[1], pos2[1]) - min(pos1[1], pos2[1])
+def distance(pos1: tuple, pos2: tuple):
+    '''Returns the Manhatten-Distance of two points.'''
+    x_dist = pos1[0] - pos2[0] if pos1[0] > pos2[0] else pos2[0] - pos1[0]
+    y_dist = pos1[1] - pos2[1] if pos1[1] > pos2[1] else pos2[1] - pos1[1]
     return x_dist + y_dist
 
 
+# Coordinate system is like the one of a monitor
 # Directions as coordinates/vectors:
 VEC_UP = (0, -1)
 VEC_LEFT = (-1, 0)
@@ -14,56 +16,79 @@ VEC_DOWN = (0, 1)
 VEC_RIGHT = (1, 0)
 
 
-def spiral():
-    '''This outputs every next position of the spiral.'''
-    width, height = 1, 0
-    vec = VEC_RIGHT
-    remain = 1
-    pos = (0, 0)  # Starting point of the spiral
-    yield pos
-    while True:
-        pos = pos[0] + vec[0], pos[1] + vec[1]  # Go one into cur_dir
+def vector_turn_left(vec: tuple):
+    '''Returns the next vector that is pointing leftwards of the given one.'''
+    if vec is VEC_RIGHT:
+        return VEC_UP
+    elif vec is VEC_UP:
+        return VEC_LEFT
+    elif vec is VEC_LEFT:
+        return VEC_DOWN
+    elif vec is VEC_DOWN:
+        return VEC_RIGHT
+    else:
+        raise Exception('Invalid Vector!')
+
+
+def spiral(skip_to_step=None):
+    '''Generates all positions of the spiral. If 'skip_to_step' is given,
+    all positions except the final position to be about 57 times faster.'''
+    pos, vec, width, height, remain = (0, 0), VEC_RIGHT, 1, 0, 1
+    if not skip_to_step:
         yield pos
-        remain -= 1
+
+    # Only necessary for the use of 'skip_to_step':
+    target_pos, step = None, 1
+    while True:
+        # Skip steps if allowed and below the target step (skip_to_step):
+        if target_pos and skip_to_step and step + remain <= skip_to_step:
+            step += remain
+            remain = 0
+            pos = target_pos
+        else:
+            pos = pos[0] + vec[0], pos[1] + vec[1]  # Go one step
+            step += 1
+            remain -= 1
+            if not skip_to_step:
+                yield pos
+
+        # Return last pos and end generator when target step is reached:
+        if skip_to_step == step:  # The 'is'-operator will not work here
+            yield pos
+            return  # End generator
+
         if remain is 0:  # Turn left (spiral direction)
-            if vec == VEC_RIGHT:
-                vec = VEC_UP
-                height += 1
-            elif vec == VEC_UP:
-                vec = VEC_LEFT
+            vec = vector_turn_left(vec)
+            if vec[0] is not 0:  # Vector is left or right
                 width += 1
-            elif vec == VEC_LEFT:
-                vec = VEC_DOWN
+            elif vec[1] is not 0:  # Vector is up or down
                 height += 1
-            elif vec == VEC_DOWN:
-                vec = VEC_RIGHT
-                width += 1
+
             target_pos = (pos[0] + (width * vec[0]), pos[1] + (height * vec[1]))
             remain = distance(pos, target_pos)
 
 
 def solve_part_1(puzzle_input):
-    target_index = int(puzzle_input) - 1
-    log('Target is: %d' % (target_index + 1))
-    for index, pos in enumerate(spiral()):
-        if index == target_index:
-            log('Target Position: %s' % str(pos))
-            return distance([0, 0], pos)
+    target_step = int(puzzle_input)
+    return distance((0, 0), next(spiral(skip_to_step=target_step)))
 
 
-def adjecents(pos):
+def adjecents_of(pos: tuple):
+    '''Generates all positions around a given one.'''
     for y in range(-1, 2):  # -> -1, 0, 1
         for x in range(-1, 2):  # -> -1, 0, 1
             yield pos[0] + y, pos[1] + x
 
 
 def put_value(infinite_matrix: dict, pos: tuple, value):
+    '''Puts value into a 2D-Dict with no fixed size (called infinite_matrix)'''
     if pos[0] not in infinite_matrix:  # Create dict for y-values
         infinite_matrix[pos[0]] = {}
     infinite_matrix[pos[0]][pos[1]] = value
 
 
 def get_value(infinite_matrix: dict, pos: tuple):
+    '''Retreives value from the 'infinite_matrix'.'''
     # Check if coordante is existing:
     if pos[0] not in infinite_matrix or pos[1] not in infinite_matrix[pos[0]]:
         return None
@@ -75,12 +100,11 @@ def solve_part_2(puzzle_input):
     values = {}  # infinite_matrix (used with the two functions above)
     put_value(values, (0, 0), 1)
     for index, pos in enumerate(spiral()):
-        value = 0
-        for adjecent_pos in adjecents(pos):
-            adjecent_pos_value = get_value(values, adjecent_pos)
-            if adjecent_pos_value:
-                value += adjecent_pos_value
-        if value > puzzle_input:
-            log('Target Position: %s' % str(pos))
-            return value
-        put_value(values, pos, value)
+        value_sum = 0  # Sum of the values of all adjecent positions
+        for adjecent_pos in adjecents_of(pos):
+            adj_value = get_value(values, adjecent_pos)
+            if adj_value:
+                value_sum += adj_value
+        if value_sum > puzzle_input:
+            return value_sum
+        put_value(values, pos, value_sum)
