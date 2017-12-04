cb1f1e175472c21b90e60f9ec981d3d4d3309a6f
LinusCDE <wendefelix1998@gmail.com>
2017-12-04 07:41:24 +0100

    Added solution for day 4 part 2

--- puzzle4.py
+++ puzzle4.py
@@ -1,20 +1,36 @@
 log = None
 
 
-def valid(phrase):
+POLICY_NO_DUPLICATES = 0
+POLICY_NO_ANAGRAMS = 1
+
+
+def valid(phrase, policies):
     words = []
     for word in phrase.split(' '):
-        if word in words:
+        if POLICY_NO_ANAGRAMS in policies:
+            word = list(word)
+            word.sort()
+            word = str().join(word)
+        if word in words and POLICY_NO_DUPLICATES in policies:
             return False
         words.append(word)
     return True
 
-def solve_part_1(puzzle_input):
+
+def count(puzzle_input, policies):
     valid_count = 0
     for phrase in puzzle_input.split('\n'):
-        if valid(phrase):
+        if valid(phrase, policies):
             valid_count += 1
     return valid_count
 
+
+def solve_part_1(puzzle_input):
+    return count(puzzle_input, [POLICY_NO_DUPLICATES])
+
+
+
 def solve_part_2(puzzle_input):
-    pass
+    return count(puzzle_input, [POLICY_NO_DUPLICATES, POLICY_NO_ANAGRAMS])
+
