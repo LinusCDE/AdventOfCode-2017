c24ef7d3668be4ab28895a94ac51411f36f67dc2
Linus <wendefelix1998@gmail.com>
2017-12-04 16:37:09 +0100

    Restructured code for day 4 and increased performance a little bit

--- puzzle4.py
+++ puzzle4.py
@@ -1,36 +1,22 @@
-log = None
-
-
-POLICY_NO_DUPLICATES = 0
-POLICY_NO_ANAGRAMS = 1
-
-
-def phrase_valid(phrase, policies):
-    '''Ckecks if the given phrase satisfies all given policies'''
-    words = []  # Will contain already found words
-    for word in phrase.split(' '):
-        if POLICY_NO_ANAGRAMS in policies:
-            word = str().join(sorted(word))  # Sorts all letters alphabetically
-        if word in words and POLICY_NO_DUPLICATES in policies:
-            return False  # Word was already found
-        words.append(word)
-    return True
-
-
-def count_valid_phrases(puzzle_input, policies):
+def count_valid_phrases(puzzle_input, check_anagrams=False):
     '''Counts the amount of valid phrases'''
     valid = 0
     for phrase in puzzle_input.split('\n'):
-        if phrase_valid(phrase, policies):
+        words = []  # Will contain already found words
+        for word in phrase.split():
+            if check_anagrams:
+                word = str().join(sorted(word))  # Sorts all letters alphabetically
+            if word in words:
+                break  # Word was already found
+            words.append(word)
+        else:  # No duplicates ((and anagrams)) found
             valid += 1
     return valid
 
 
 def solve_part_1(puzzle_input):
-    return count_valid_phrases(puzzle_input, [POLICY_NO_DUPLICATES])
-
+    return count_valid_phrases(puzzle_input)
 
 
 def solve_part_2(puzzle_input):
-    return count_valid_phrases(puzzle_input, [POLICY_NO_DUPLICATES, POLICY_NO_ANAGRAMS])
-
+    return count_valid_phrases(puzzle_input, check_anagrams=True)
