7a939c21e48c01e7f547c867a91f540b3f0f3f87
LinusCDE <wendefelix1998@gmail.com>
2017-12-04 07:58:12 +0100

    Optimized code and commented it

--- puzzle4.py
+++ puzzle4.py
@@ -5,32 +5,32 @@ POLICY_NO_DUPLICATES = 0
 POLICY_NO_ANAGRAMS = 1
 
 
-def valid(phrase, policies):
-    words = []
+def phrase_valid(phrase, policies):
+    '''Ckecks if the given phrase satisfies all given policies'''
+    words = []  # Will contain already found words
     for word in phrase.split(' '):
         if POLICY_NO_ANAGRAMS in policies:
-            word = list(word)
-            word.sort()
-            word = str().join(word)
+            word = str().join(sorted(word))  # Sorts all letters alphabetically
         if word in words and POLICY_NO_DUPLICATES in policies:
-            return False
+            return False  # Word was already found
         words.append(word)
     return True
 
 
-def count(puzzle_input, policies):
-    valid_count = 0
+def count_valid_phrases(puzzle_input, policies):
+    '''Counts the amount of valid phrases'''
+    valid = 0
     for phrase in puzzle_input.split('\n'):
-        if valid(phrase, policies):
-            valid_count += 1
-    return valid_count
+        if phrase_valid(phrase, policies):
+            valid += 1
+    return valid
 
 
 def solve_part_1(puzzle_input):
-    return count(puzzle_input, [POLICY_NO_DUPLICATES])
+    return count_valid_phrases(puzzle_input, [POLICY_NO_DUPLICATES])
 
 
 
 def solve_part_2(puzzle_input):
-    return count(puzzle_input, [POLICY_NO_DUPLICATES, POLICY_NO_ANAGRAMS])
+    return count_valid_phrases(puzzle_input, [POLICY_NO_DUPLICATES, POLICY_NO_ANAGRAMS])
 
