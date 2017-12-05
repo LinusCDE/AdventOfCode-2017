f37bcdd64cd84a351751842491c5c0b8a67525a5
Linus <wendefelix1998@gmail.com>
2017-12-05 13:06:54 +0000

    Improved performance

--- puzzle5.py
+++ puzzle5.py
@@ -1,22 +1,24 @@
 def solve_part_1(puzzle_input):
     jump_list = list(map(int, puzzle_input.split('\n')))
-    current_index = 0
-    jump_count = 0
-    while current_index >= 0 and current_index < len(jump_list):
-        index = current_index
-        current_index += jump_list[current_index]
+    size, current_index, jump_count = len(jump_list), 0, 0
+    while True:
+        index, instruction = current_index, jump_list[current_index]
+        current_index += instruction
         jump_list[index] += 1
         jump_count += 1
+        if current_index < 0 or current_index >= size:
+            break
     return jump_count
 
 
 def solve_part_2(puzzle_input):
     jump_list = list(map(int, puzzle_input.split('\n')))
-    current_index = 0
-    jump_count = 0
-    while current_index >= 0 and current_index < len(jump_list):
-        index = current_index
-        current_index += jump_list[current_index]
-        jump_list[index] += 1 if jump_list[index] < 3 else -1
+    size, current_index, jump_count = len(jump_list), 0, 0
+    while True:
+        index, instruction = current_index, jump_list[current_index]
+        current_index += instruction
+        jump_list[index] += 1 if instruction < 3 else -1
         jump_count += 1
+        if current_index < 0 or current_index >= size:
+            break
     return jump_count
