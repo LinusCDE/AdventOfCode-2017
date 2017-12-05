154277a8da47b5dfbd4b502f4c6531b4a720f47e
Linus <wendefelix1998@gmail.com>
2017-12-05 13:14:18 +0000

    Improved performance #2

--- puzzle5.py
+++ puzzle5.py
@@ -1,13 +1,13 @@
 def solve_part_1(puzzle_input):
-    jump_list = list(map(int, puzzle_input.split('\n')))
-    size, current_index, jump_count = len(jump_list), 0, 0
-    while True:
-        index, instruction = current_index, jump_list[current_index]
-        current_index += instruction
-        jump_list[index] += 1
-        jump_count += 1
-        if current_index < 0 or current_index >= size:
-            break
+    jump_list = list(map(int, puzzle_input.split('\n')))  # Input as int-list
+    size, current_index, jump_count = len(jump_list), 0, 0  # Needed variables
+    while True:  # Following instructions
+        instruction = jump_list[current_index]  # Get the instruction
+        jump_list[current_index] += 1  # Change used instruction
+        current_index += instruction  # Jump to new index
+        jump_count += 1  # Step counting
+        if current_index < 0 or current_index >= size:  # Check if in list
+            break  # No more in list. Finished
     return jump_count
 
 
@@ -15,9 +15,11 @@ def solve_part_2(puzzle_input):
     jump_list = list(map(int, puzzle_input.split('\n')))
     size, current_index, jump_count = len(jump_list), 0, 0
     while True:
-        index, instruction = current_index, jump_list[current_index]
+        instruction = jump_list[current_index]
+        # Change to part 1:
+        jump_list[current_index] += 1 if instruction < 3 else -1
+        # ----------
         current_index += instruction
-        jump_list[index] += 1 if instruction < 3 else -1
         jump_count += 1
         if current_index < 0 or current_index >= size:
             break
