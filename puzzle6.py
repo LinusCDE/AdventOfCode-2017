74a03cb5d434c44b08cfecfb0ff9e35f7a0a2127
Linus <wendefelix1998@gmail.com>
2017-12-06 14:09:06 +0000

    Some optimizations

--- puzzle6.py
+++ puzzle6.py
@@ -4,23 +4,26 @@ from itertools import count
 def find_duplicate_allocation(banks: str):
     '''Returns the reallocation count as well as the index of the duplicated
      bank that was found.'''
-    banks_history = list()  # Contains all previous banks as tuples
+    banks_history = list()  # Contains all previous banks as lists
     banks = list(map(int, banks.split()))
     total_banks = len(banks)
 
     for reallocations in count(1):
-        biggest_val = max(banks)
-        biggest_at = banks.index(biggest_val)
+        biggest_val, biggest_at = 0, 0
+        for index, val in enumerate(banks):
+            if val > biggest_val:
+                biggest_val = val
+                biggest_at = index
 
         banks[biggest_at] = 0  # Clear current bank
         # Re-distribute the all blocks on the banks:
         for index in range(biggest_at + 1, biggest_at + biggest_val + 1):
             banks[index % total_banks] += 1
-        banks_tuple = tuple(banks)  # Save new bank allocations as tuple
-        if banks_tuple in banks_history:
-            seen_at = banks_history.index(banks_tuple)
+
+        if banks in banks_history:
+            seen_at = banks_history.index(banks)
             return (reallocations, seen_at + 1)
-        banks_history.append(banks_tuple)
+        banks_history.append(banks.copy())
 
 
 def solve_part_1(puzzle_input):
