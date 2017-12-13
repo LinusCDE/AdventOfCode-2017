5a5e08992ce83c94964a1f202854896bdc2990c4
Linus <wendefelix1998@gmail.com>
2017-12-13 16:58:01 +0100

    Overhauled and commented code

--- puzzle13.py
+++ puzzle13.py
@@ -1,74 +1,50 @@
 from itertools import count
 
 
-class Layer:
-
-    def __init__(self, depth: int, layer_range: int):
-        self.depth, self.range = depth, layer_range
-
-    def position(self, ticks: int):
-        '''
-        Source: https://stackoverflow.com/a/11544567/3949509
-        '''
-        input_range = self.range - 1
-        return abs(((ticks + input_range) % (input_range * 2)) - input_range)
-
-
 class Firewall:
+    '''Representation of the firewall in the puzzle.'''
 
     def __init__(self, puzzle_input: str):
-        self.ticks = 0
-        self.layers = {}  # Format: {depth: Layer(...)}
+        self.ticks = 0  # Current picoseconds
+        self.range_by_depth = dict()  # Format: {depth: range}
         self.max_depth = 0
+
+        # Fill 'self.max_depth' and 'self.range_by_depth':
         for line in puzzle_input.split('\n'):
             depth, layer_range = map(int, line.split(': '))
-            self.layers[depth] = Layer(depth, layer_range)
+            self.range_by_depth[depth] = layer_range
             self.max_depth = max(self.max_depth, depth)
 
-    def tick(self, steps: int=1):
-        self.ticks += steps
+    def tick(self):
+        '''Increments one tick.'''
+        self.ticks += 1
 
-    def can_pass(self, depth: int) -> bool:
-        if depth not in self.layers:
-            return True
+    def layer_position(self, depth) -> int:
+        '''Returns the current layer position.
+        If no layer exists, -1 will be returned.
+        '''
+        if depth not in self.range_by_depth:
+            return -1
 
-        return self.layers[depth].position(self.ticks) != 0
-
-    def reset(self):
-        for layer in self.layers.values():
-            layer.direction = 1
-            layer.position = 0
-
-    def positions(self):
-        pos = {}
-        for layer in self.layers.values():
-            pos[layer.depth] = layer.position
-        return pos
-
-
-def load(puzzle_input):
-    max_depth = 0
-    layers = {}  # Format: {depth: {pos: int, dir: int, range: int}, ...}
-    for line in puzzle_input.split('\n'):
-        depth, layer_range = map(int, line.split(': '))
-        layers[depth] = {'pos': 0, 'dir': 1, 'range': layer_range}
-        max_depth = max(max_depth, depth)
-    return max_depth, layers
+        # Source: https://stackoverflow.com/a/11544567/3949509
+        layer_range = self.range_by_depth[depth] - 1
+        return abs(((self.ticks + layer_range) % (layer_range * 2)) - layer_range)
 
 
 def solve_part_1(puzzle_input):
     firewall = Firewall(puzzle_input)
     serverity = 0
     for depth in range(firewall.max_depth + 1):
-        if not firewall.can_pass(depth):
-            serverity += (depth * firewall.layers[depth].range)
+        if firewall.layer_position(depth) == 0:
+            serverity += (depth * firewall.range_by_depth[depth])
         firewall.tick()
     return serverity
 
 
-def caught_and_reset(firewall: Firewall) -> int:
+def caught(firewall: Firewall) -> bool:
+    '''Checks if the program would be caught in the 'firewall'.'''
     for depth in range(firewall.max_depth + 1):
-        if not firewall.can_pass(depth):
+        if firewall.layer_position(depth) == 0:
             return True
         firewall.tick()
     return False
@@ -77,6 +53,7 @@ def caught_and_reset(firewall: Firewall) -> int:
 def solve_part_2(puzzle_input):
     firewall = Firewall(puzzle_input)
     for ticks_delayed in count():
+        # Try with different delays:
         firewall.ticks = ticks_delayed
-        if not caught_and_reset(firewall):
+        if not caught(firewall):
             return ticks_delayed
