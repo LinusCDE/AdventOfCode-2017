ddcd29423df0a450fca1502630343e6810a8b1f7
Linus <wendefelix1998@gmail.com>
2017-12-15 13:49:24 +0000

    Fixed bitwise comparing which lead to a heavy increase in performance.

--- puzzle15.py
+++ puzzle15.py
@@ -1,8 +1,6 @@
-log = None
 FACTOR_A, FACTOR_B = 16807, 48271
 
-print('Hint: Run this puzzle with \'pypy3\' to get vastly improved '
-      'performance.')
+print('Hint: Run this puzzle with \'pypy3\' about 33% more performance.')
 
 
 def generator(value: int, factor: int):
@@ -10,18 +8,6 @@ def generator(value: int, factor: int):
     while True:
         value = (value * factor) % 2147483647
         yield value
-
-
-def lowest_16_bits(iterateable):
-    '''Returns the last 16 bits for each number in 'iterable'.'''
-    for number in iterateable:
-        bits = bin(number)[2:]
-        bits_len = len(bits)
-
-        if bits_len > 16:
-            yield bits[bits_len-16:]
-        else:
-            yield bits
 
 
 def parse_start_values(puzzle_input):
@@ -38,24 +24,12 @@ def parse_start_values(puzzle_input):
     return map(int, (start_a, start_b))  # Returns the startnumbers as ints
 
 
-def count(bit_gen_a, bit_gen_b, cycles):
+def count(generator_a, generator_b, cycles):
     '''Returns how often the judge successfully found fitting last 16 bits.'''
     count = 0  # The count
-    output_count = 0  # Count for verbosity-puroses
 
-    for index, bits_a, bits_b in zip(range(cycles),
-                                     lowest_16_bits(bit_gen_a),
-                                     lowest_16_bits(bit_gen_b)):
-        # Print progress every half million cycles:
-        output_count += 1
-        if output_count > 500000:
-            output_count = 0
-            log('Progress: %d' % ((index+1) * 100 / cycles) + '%')
-
-        # Pretty shitty comparison of the last 16 bits:
-        if len(bits_a) > len(bits_b) and bits_a.endswith(bits_b):
-            count += 1
-        elif bits_b.endswith(bits_a):
+    for index, num_a, num_b in zip(range(cycles), generator_a, generator_b):
+        if (num_a & 0xFFFF) == (num_b & 0xFFFF):
             count += 1
 
     return count
@@ -64,16 +38,16 @@ def count(bit_gen_a, bit_gen_b, cycles):
 def solve_part_1(puzzle_input):
     start_a, start_b = parse_start_values(puzzle_input)
 
-    numbers_a = generator(start_a, FACTOR_A)
-    numbers_b = generator(start_b, FACTOR_B)
+    generator_a = generator(start_a, FACTOR_A)
+    generator_b = generator(start_b, FACTOR_B)
 
-    return count(numbers_a, numbers_b, 40000000)  # 40 Million cycles
+    return count(generator_a, generator_b, 40000000)
 
 
 def solve_part_2(puzzle_input):
     start_a, start_b = parse_start_values(puzzle_input)
 
-    numbers_a = filter(lambda val: val % 4 == 0, generator(start_a, FACTOR_A))
-    numbers_b = filter(lambda val: val % 8 == 0, generator(start_b, FACTOR_B))
+    generator_a = filter(lambda val: val % 4 == 0, generator(start_a, FACTOR_A))
+    generator_b = filter(lambda val: val % 8 == 0, generator(start_b, FACTOR_B))
 
-    return count(numbers_a, numbers_b, 5000000)  # 5 Million cycles with filter
+    return count(generator_a, generator_b, 5000000)
