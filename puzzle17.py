f7e152ce06ce6c712e8e5f2ff594cef5f9d58c6d
Linus <wendefelix1998@gmail.com>
2017-12-17 21:28:25 +0100

    Overhauled and commented code

--- puzzle17.py
+++ puzzle17.py
@@ -1,24 +1,35 @@
-def spin(value_count=2018, steps=3) -> tuple:
+print('Hint: Run this puzzle with \'pypy3\' to get improved '
+      'performance in part 2.')
+
+
+def spin(steps, inserts=2018) -> tuple:
+    '''Returns the buffer and last index after given
+    insertions ('inserts') using given 'steps' to increment.
+     '''
     buffer, index = [0], 0
-    for inserting in range(1, value_count):
+    for inserting in range(1, inserts):
         index = ((index + steps) % inserting) + 1
+
+        # 'list.insert()' inserts after the given index
+        # hence the '+ 1'ed index becomes the correct one.
         buffer.insert(index, inserting)
-        print(buffer.index(1))
     return buffer, index
 
 
 def solve_part_1(puzzle_input):
     buffer, index = spin(steps=int(puzzle_input))
-    print(buffer)
     return buffer[(index + 1) % len(buffer)]
 
 
 def solve_part_2(puzzle_input):
-    index = 0
-    final = -1
     steps = int(puzzle_input)
-    for inserting in range(1, 50000000):
+
+    index, result = 0, 0
+    # Didn't use 50_000_000 because pypy3 doesn't support underscores (_).
+    for inserting in range(1, 50000000):  # Also the current buffer-size
         index = ((index + steps) % inserting) + 1
-        if index == 1:
-            final = inserting
-    return final
+
+        if index == 1:  # The zero in buffer will always remain at index 0
+            result = inserting
+
+    return result
