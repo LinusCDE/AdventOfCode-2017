2d1c8b6e3f0e39d4d0e0fc2e92a0a77091f1d702
Linus <wendefelix1998@gmail.com>
2017-12-17 21:17:18 +0100

    Solved day 17 part 2

--- puzzle17.py
+++ puzzle17.py
@@ -1,18 +1,24 @@
 def spin(value_count=2018, steps=3) -> tuple:
-    buffer, index, last_inserted = [0], 0, 0
-    for _ in range(value_count - 1):
-        index += steps
-        index %= len(buffer)
-        last_inserted += 1
-        buffer.insert(index + 1, last_inserted)
-        index += 1
+    buffer, index = [0], 0
+    for inserting in range(1, value_count):
+        index = ((index + steps) % inserting) + 1
+        buffer.insert(index, inserting)
+        print(buffer.index(1))
     return buffer, index
 
 
 def solve_part_1(puzzle_input):
     buffer, index = spin(steps=int(puzzle_input))
+    print(buffer)
     return buffer[(index + 1) % len(buffer)]
 
 
 def solve_part_2(puzzle_input):
-    pass
+    index = 0
+    final = -1
+    steps = int(puzzle_input)
+    for inserting in range(1, 50000000):
+        index = ((index + steps) % inserting) + 1
+        if index == 1:
+            final = inserting
+    return final
