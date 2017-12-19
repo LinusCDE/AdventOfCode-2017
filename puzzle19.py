fddf5850caa76a5efdfef7bf73ce8694ed50ccad
Linus <wendefelix1998@gmail.com>
2017-12-19 15:41:34 +0000

    Overhauled code for day 19 and commented it

--- puzzle19.py
+++ puzzle19.py
@@ -1,95 +1,101 @@
-from coordinate_utils import CoordinateField
-from puzzle3 import VEC_UP, VEC_RIGHT, VEC_DOWN, VEC_LEFT
+from coordinate_utils import CoordinateField, add, direction
+
+
+# Argument for aoc.py. When set to False the puzzle_input doesn't
+# get stripped, which would destroy some coordinates:
+AOC_STRIP_INPUT = False
 
 
 def load_route(puzzle_input) -> CoordinateField:
+    '''Puts the content of 'puzzle_input' into a CordinateField
+    and returns it.
+    '''
     field = CoordinateField()
     highest_x, highest_y = 0, 0
+
     for y, line in enumerate(puzzle_input.split('\n')):
         highest_y = y
         for x, char in enumerate(line):
             highest_x = max(highest_x, x)
             field[x, y] = char
 
-    field._infinite = False
-    field._min_x, field._min_y = 0, 0
-    field._max_x, field._max_y = highest_x, highest_y
+    field.set_size(0, highest_x, 0, highest_y)
     return field
 
 
-def directions(char):
-    if char == '|':
-        return (VEC_DOWN, VEC_UP)
-    elif char == '-':
-        return (VEC_LEFT, VEC_RIGHT)
-
-
-def is_road(char):
-    return char in ('|', '-')
-
-
-def is_letter(char):
+def is_letter(char: str):
+    '''Returns whether the character ('char') is one of the searched letters.'''
     if not isinstance(char, str):
         return False
     return char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
-def find_next(field: CoordinateField, pos, last_pos):
-    char = field[pos]
-    dirs = directions(char)
-    #print(dirs)
-    if dirs is not None or is_letter(char):
-        #print('Vec')
-        vector = pos[0] - last_pos[0], pos[1] - last_pos[1]
-        return pos[0] + vector[0], pos[1] + vector[1]
-    if char == '+':
-        print('TURN')
-        for possible in field.adjectents(pos, diagonals=False):
-            nchar = field[possible]
-            if possible != last_pos \
-             and (directions(nchar) is not None or is_letter(nchar)):
-                return possible
-    raise Exception('No further position found! (%d, %d: %s)'
-                    % (pos[0], pos[1], field[pos]))
+def is_road(char: str):
+    '''Returns whether this character ('char') is part of a road.
+    A road can be '|', '-' or any uppercase letter (see 'is_letter(char)').'''
+    return char in ('|', '-') or is_letter(char)
 
 
-def find_entry_point(field: CoordinateField):
+def follow(field: CoordinateField, pos):
+    '''Finds and yields all positions and their values,
+    following 'pos' downwards.
+    '''
+    last_pos = add(pos, (0, -1))  # Inital direction is: down
+
+    while True:  # Will be left manually
+        char = field[pos]  # Char at curren position
+        yield pos[0], pos[1], char
+
+        if is_road(char):  # Move forward the same direction
+            next_pos = add(pos, direction(last_pos, pos))
+
+            # Finish if out of coordinate field or nothing (space) found:
+            if not field.in_field(next_pos) or \
+               (not is_road(field[next_pos]) and field[next_pos] != '+'):
+                return  # Finished
+        else:
+            if char != '+':
+                raise Exception('Unexpected character: %s' % char)
+
+            # Encountered a '+'. Need to choose new direction:
+            for possible in field.adjectents(pos, diagonals=False):
+                nchar = field[possible]
+                if possible != last_pos and is_road(nchar):
+                    next_pos = possible
+                    break
+            else:
+                raise Exception('No direction found in which to turn.')
+
+        last_pos, pos = pos, next_pos
+
+
+def find_entry_position(field: CoordinateField) -> tuple:
+    '''Determines and returns entry point of the coordinate field.'''
     for x, y, value in field.items():
-        print(x, y, '%s' % value)
-        if value is not None and value != ' ' and y == 0:
+        if is_road(value) and y == 0:
             return x, y
     else:
-        raise Exception('No Entry found!')
+        raise Exception('No entry position found!')
 
 
 def solve_part_1(puzzle_input):
-    with open('/tmp/input.txt') as file:
-        puzzle_input = file.read()
     field = load_route(puzzle_input)
-    values = []
-    pos = None
 
-    pos = find_entry_point(field)
-    last_pos = pos[0], pos[1] - 1
-    c = 0
-    while True:
-        c += 1
-        try:
-            next_pos = find_next(field, pos, last_pos)
-            print(next_pos)
-            if field[next_pos] is None or field[next_pos] == ' ':
-                print(next_pos)
-                print('Part 2: %d' % c)
-                return ''.join(values)
+    entry_pos, letters = find_entry_position(field), []
 
-            if is_letter(field[next_pos]):
-                values.append(field[next_pos])
-            last_pos, pos = pos, next_pos
-        except Exception as e:  # Coordinate fail (outside of bounds)
-            print(e.message)
-            print('Part 2: %d' % c)
-            return ''.join(values)
+    for x, y, char in follow(field, entry_pos):
+        if is_letter(char):
+            letters.append(char)
+
+    return ''.join(letters)
 
 
 def solve_part_2(puzzle_input):
-    pass
+    field = load_route(puzzle_input)
+
+    entry_pos, count = find_entry_position(field), 0
+
+    for _ in follow(field, entry_pos):
+        count += 1
+
+    return count
